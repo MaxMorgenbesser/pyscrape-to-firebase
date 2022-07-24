f7195a7e82b22b45cb68f1762c479adb3e664a4f
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-07-24 17:44:13 -0400

    Create option will set a collection in firebase

--- venv/main.py
+++ venv/main.py
@@ -4,19 +4,18 @@ import pandas as pd
 from requests_html import HTML
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import db
+from firebase_admin import firestore
+from firebase import firebase
 from requests_html import HTMLSession
 
-URL = input('Please enter the URL of the site you would like to get information from: ')
+#url = input('Please enter the URL of the site you would like to get information from: ')
 #connecting to firebase
 
 cred = credentials.Certificate('credentials.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL': "URL to database"
-})
+firebase_admin.initialize_app(cred)
+db=firestore.client()
+firebase = firebase.FirebaseApplication('https://scraping-practice-py.firebaseio.com', None)
 
-
-#gets the url that can be used to find data from
 
 #URL = input('Please enter the URL of the site you would like to get information from: ')
 
@@ -29,25 +28,53 @@ firebase_admin.initialize_app(cred, {
 # print(getSource(url))
 
 #connects to specified url
-session=HTMLSession()
-response= session.get(url)
+# session=HTMLSession()
+# response= session.get(url)
 # returns the title of the page of the url that is passed
-title = response.html.find('title', first=True).text
-print(title)
-ref = db.reference('Database reference')
+# title = response.html.find('title', first=True).text
+# print(title)
+# ref = db.reference('Database reference')
 
 #canonical link is one used to optimize search engine searches(like a shortened version)
-canonical = response.html.xpath("//link[@rel='canonical']/@href")
-print(canonical)
+#canonical = response.html.xpath("//link[@rel='canonical']/@href")
+# print(canonical)
 
 #returns description of a page if one has one
-description = response.html.xpath("//meta[@name='description']/@content")
+# description = response.html.xpath("//meta[@name='description']/@content")
 
 
 #returns author of a page
-author =  response.html.xpath("//meta[@name='author']/@content")
-print(author + description)
+#author =  response.html.xpath("//meta[@name='author']/@content")
+#print(author + description)
 
 #gets image link from page if one has one
-image=response.html.xpath("//meta[@property='og:image']/@content")
-print(image)
+#image=response.html.xpath("//meta[@property='og:image']/@content")
+# print(image)
+
+def create():
+    url = input('Please enter the URL of the site you would like to get information from: ')
+    session = HTMLSession()
+    response = session.get(url)
+    title = response.html.find('title', first=True).text
+    description = response.html.xpath("//meta[@name='description']/@content")
+    canonical = response.html.xpath("//link[@rel='canonical']/@href")
+    author = response.html.xpath("//meta[@name='author']/@content")
+    image = response.html.xpath("//meta[@property='og:image']/@content")
+    collectName=input("Please enter the name of the collection you would like to create. \n")
+    db.collection(collectName).document(title).set(
+        {'Title': title,
+         'Author': author,
+         'Description': description,
+         'Image': image,
+         'Canonical link': canonical
+         }
+    )
+
+def main():
+    doCrud='Y'
+    while doCrud.upper()=='Y':
+        crud=input("Would you like to Create,Read,Update or Delete?\n (c,r,u,d)")
+        if crud.upper()=='C':
+            create()
+            doCrud=input('Would you like to create, read, update or delete anything else?\n(y/n): ')
+main()
